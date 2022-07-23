848ceedf0f293851c5e37eb5fcdf13e7bfd0c7b6
sleepyfive <1349945922@qq.com>
2022-07-23 13:51:20 +0800

    wip: button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import React from 'react'
 import { IconArrowDown } from './icons'
+import Button from "./components/button";
+
+const components = [
+	{
+		title: 'icon',
+		comp: <IconArrowDown style={{
+			color:'red',
+			width: '50px',
+			height: '50px'
+		}}/>
+	},
+	{
+		title: 'button',
+		comp: (<div>
+				<Button type='primary'>Primary</Button>
+				<Button type='secondary'>Secondary</Button>
+				<Button type='dashed'>Dashed</Button>
+				<Button type='outline'>Outline</Button>
+				<Button type='text'>Text</Button>
+			</div>)
+	}
+]
 
 const App = ()=>{
 	return (
 		<div>
-			2333
-			<IconArrowDown style={{
-				color:'red',
-				width: '50px',
-				height: '50px'
-			}}/>
+			{
+				components.reverse().map(e=>{
+					return (
+						<div>
+							<h2>{e.title}</h2>
+							{e.comp}
+						</div>
+					)
+				})
+			}
 		</div>
 	)
 }
